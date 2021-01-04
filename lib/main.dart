960f3572783cd4781817cea0d240905def2311ee
Murli Manohar K <murli6619@gmail.com>
2021-01-04 14:58:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+}
+class MyAppState extends State
+{
+  var questionIndex = 0;
+  void answer(){
+      questionIndex = questionIndex + 1;
+      print(questionIndex);
+    }
   @override
   Widget build(BuildContext context) {
+    
+    var questions = [
+      'What\'s your name',
+      'What\'s your DOB'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz Application'),
         ),
-        body: Text('This is the quiz app'),
+        body: Column(children: [
+          Text(
+            questions[questionIndex],
+          ),
+          RaisedButton(child: Text('Ans 1'), onPressed: answer),
+          RaisedButton(child: Text('Ans 2'), onPressed: answer),
+          RaisedButton(child: Text('Ans 3'), onPressed: answer),
+        ]),
       ),
     );
   }
